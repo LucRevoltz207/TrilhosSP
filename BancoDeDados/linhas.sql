89ab8934fcfa8b484aab336915742e96305210b9
O Moura <123185795+LucRevoltz207@users.noreply.github.com>
2024-01-19 00:01:57 -0300

    up 2

---
+++ BancoDeDados/linhas.sql
@@ -0,0 +1,15 @@
+INSERT INTO linha (Id_Linha, Nome_Linha)
+VALUES
+(1,'Azul'),
+(2,'Verde'),
+(3,'Vermelho'),
+(4,'Amarela'),
+(5,'Lilas'),
+(7,'Rubi'),
+(8,'Diamante'),
+(9,'Esmeralda'),
+(10,'Turquesa'),
+(11,'Coral'),
+(12,'Safira'),
+(13,'Jade'),
+(15,'Prata');
